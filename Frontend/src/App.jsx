c4033a142a2d6a363f01159c956a6331a9863772
astha013 <asthapawar0138@gmail.com>
2025-05-09 00:15:47 +0530

    Initial commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    
+      <div className="bg-black-500 text-white p-4">
+        Tailwind is working!
+      </div>
+
         
     </>
   )
